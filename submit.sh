90a4d4c06f4a93ea65e2b3fbb06c27db9d3a114d
Claire <guerin.claire01@gmail.com>
2017-07-14 20:03:43 +0200

    Clean up bash file and add srcipt name as trackchicks

--- submit.sh
+++ submit.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH -J <SCRIPT> 	# name of the script to run
-#SBATCH -o /n/regal/debivort_lab/claire/…/<SCRIPT>.out
-#SBATCH -e /n/regal/debivort_lab/claire/…/<SCRIPT>.err
-#SBATCH -N 1 		# number of machines – keep to a single one!!!
-#SBATCH -c 36 		# number of cores requested
-#SBATCH -t 0-12:00 	# D-HH-MM
-#SBATCH -p general 	# port on which to run the job
-#SBATCH --mem=32000	# maximum memory
-#SBATCH --mail-type=END	# send email once the job is completed
+#SBATCH -J trackchicks
+#SBATCH -o /n/regal/debivort_lab/claire/pulcini/trackchicks.out
+#SBATCH -e /n/regal/debivort_lab/claire/pulcini/trackchicks.err
+#SBATCH -N 1 		
+#SBATCH -c 36 		
+#SBATCH -t 0-12:00 	
+#SBATCH -p general 	
+#SBATCH --mem=32000	
+#SBATCH --mail-type=END
 #SBATCH --mail-user=guerin.claire01@gmail.com
 
 mkdir -p /scratch/$USER/$SLURM_JOB_ID
 
-srun -c 36 matlab-default -nosplash -nodesktop -r "<SCRIPT>;exit"
+srun -c 36 matlab-default -nosplash -nodesktop -r "trackchicks;exit"
 
 rm -rf /scratch/$USER/$SLURM_JOB_ID
