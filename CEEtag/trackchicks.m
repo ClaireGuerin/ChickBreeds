6932aed163eb98a6801776b629cf914c7ec7effa
Claire <guerin.claire01@gmail.com>
2017-07-18 17:43:35 +0200

    Add try catch in case tag file not found

--- CEEtag/trackchicks.m
+++ CEEtag/trackchicks.m
@@ -8,11 +8,19 @@ fileList = dir('/n/regal/debivort_lab/claire/pulcini/Day2/*.avi');
 % Set up parallel computing
 pc = parcluster('local') ;
 pc.JobStorageLocation = strcat('/scratch/claire/', getenv('SLURM_JOB_ID'));
-parpool(pc, 36)
+parpool(pc, 32)
 
 % Loop through files and track chicks
 for f = 1:numel(fileList)
 	fileName = fileList(f).name;
-    [trackingData, tags] = optTracking(fileName);
+    
+    try
+        [trackingData, tags] = optTracking(fileName);
+    catch
+        warning(['Tracking failed for file ',fileName,'. Assigning NaNs.']);
+        trackingData = NaN;
+        tags = NaN;
+    end
+    
     save(strcat(fileName, '_tracked.mat'), 'trackingData', 'tags')
 end
